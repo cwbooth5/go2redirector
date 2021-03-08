1fa217b6d1d406dfac7a0b02a694fec3499f4932
Bill Booth <cwbooth5@gmail.com>
2021-03-08 05:49:23 +0000

    sort keywords in descending order as done previously

--- static/js/search.js
+++ static/js/search.js
@@ -7,10 +7,8 @@
 const searchBar = document.getElementById("go2input");
 const keywordsList = document.getElementById("keywordslist");
 
-
-
 let inputKeywords = [];  // strings the user is entering in the main input box
-var keywordsArray = [];  // list of lists. [ [keywordObject, len(lists)], ...]
+var keywordsArray = [];  // list of lists. [ [keywordObject, len(lists), Clicks], ...]
 
 searchBar.addEventListener('input', (e) => {
     const searchString = e.target.value.toLowerCase();
@@ -32,7 +30,8 @@ const loadKeywords = async () => {
         for (const [key, value] of Object.entries(keywords)) {
           // item[0] is going to be the keyword string
           // item[1] is going to be the length of keyword.Links (number of links in the list)
-          keywordsArray.push([key, Object.keys(value["Links"]).length])
+          // item[2] is going to be click count on the keyword
+          keywordsArray.push([key, Object.keys(value["Links"]).length, value.Clicks]);
         }
         inputKeywords = keywordsArray;
         displayKeywords(inputKeywords);
@@ -43,19 +42,35 @@ const loadKeywords = async () => {
 
 const displayKeywords = (keywords) => {
 
+    // sort by the third element of each array (click count)
+    keywords.sort(function(a, b) {
+        var valueA, valueB;
+        valueA = a[2]; 
+        valueB = b[2];
+        if (valueA < valueB) {
+            return -1;
+        }
+        else if (valueA > valueB) {
+            return 1;
+        }
+        return 0;
+    });
+
+    keywords.reverse(); // descending
+
     const htmlString = keywords
         .map((keyword) => {
           if (keyword[1] > 1) {
             return `
             <li class="list-inline-item">
-                <a class="go2keyword go2keyword-small" href="/.${keyword[0]}" title="${keyword[1]} links" role="button">go2/${keyword[0]}</a><sup>${keyword[1]}</sup>
+                <a class="go2keyword go2keyword-small" href="/.${keyword[0]}" title="${keyword[1]} links, ${keyword[2]} clicks" role="button">go2/${keyword[0]}</a><sup>${keyword[1]}</sup>
             </li>
             `;
           }
           else {
             return `
             <li class="list-inline-item">
-                <a class="go2keyword go2keyword-small" href="/.${keyword[0]}" title="1 link" role="button">go2/${keyword[0]}
+                <a class="go2keyword go2keyword-small" href="/.${keyword[0]}" title="1 link, ${keyword[2]} clicks" role="button">go2/${keyword[0]}
             </li>
             `;
           }
